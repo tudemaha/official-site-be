10963ff32678916941d249738f237dd9c3a99e43
tudemaha <gede_mahardika88@yahoo.co.id>
2023-06-09 11:38:55 +0800

    finish delete account

--- controller/account_controller.js
+++ controller/account_controller.js
@@ -5,6 +5,7 @@ const {
 	signupValidator,
 	loginValidator,
 	editPasswordValidator,
+	deleteAccountValidator,
 } = require("../utils/validation");
 
 const signupHandler = async (req, res) => {
@@ -210,4 +211,84 @@ const editPasswordHandler = async (req, res) => {
 	});
 };
 
-module.exports = { signupHandler, loginHandler, editPasswordHandler };
+const deleteAccountHandler = async (req, res) => {
+	const reqBody = req.body;
+	const username = req.params.username.replace(":", "");
+	const authorization = req.headers.authorization.split(" ");
+
+	const reqErrors = deleteAccountValidator(reqBody);
+	if (reqErrors.length != 0) {
+		res.status(400).json({
+			status: false,
+			code: 400,
+			message: "input not valid",
+			data: {
+				errors: reqErrors,
+			},
+		});
+		return;
+	}
+
+	const validate = await checkToken(authorization.slice(-1));
+	if (
+		typeof validate == "boolean" ||
+		!validate ||
+		authorization[0] != "Bearer"
+	) {
+		res.status(401).json({
+			status: false,
+			code: 401,
+			message: "access unauthorized",
+			data: null,
+		});
+		return;
+	}
+
+	let account = await Account.findAll({
+		where: {
+			username,
+		},
+		attributes: ["password"],
+	});
+	account = account[0];
+
+	let status = false;
+	if (
+		validate.username == username &&
+		(await checkBcrypt(account.password, reqBody.password))
+	) {
+		await Account.destroy({
+			where: {
+				username,
+				password: account.password,
+			},
+		});
+		status = true;
+	}
+
+	if (!status) {
+		res.status(403).json({
+			status: false,
+			code: 403,
+			message: "forbidden access",
+			data: {
+				errors: ["invalid credentials for account to delete password"],
+			},
+		});
+		return;
+	}
+
+	res.status(200).json({
+		status: true,
+		code: 200,
+		message: "account deleted successfully",
+		data: null,
+	});
+};
+
+module.exports = {
+	signupHandler,
+	loginHandler,
+	editPasswordHandler,
+	deleteAccountHandler,
+};
--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,7 @@ const {
 	signupHandler,
 	loginHandler,
 	editPasswordHandler,
+	deleteAccountHandler,
 } = require("./../controller/account_controller");
 const { Account } = require("./../model/models");
 const { checkToken, updateToken } = require("./../utils/token");
@@ -15,79 +16,6 @@ router.use(express.json());
 router.post("/signup", signupHandler);
 router.post("/login", loginHandler);
 router.put("/:username", editPasswordHandler);
-router.delete("/:username", async (req, res) => {
-	const reqBody = req.body;
-	const username = req.params.username.replace(":", "");
-	const authorization = req.headers.authorization.split(" ");
-
-	const reqErrors = deleteAccountValidator(reqBody);
-	if (reqErrors.length != 0) {
-		res.status(400).json({
-			status: false,
-			code: 400,
-			message: "input not valid",
-			data: {
-				errors: reqErrors,
-			},
-		});
-		return;
-	}
-
-	const validate = await checkToken(authorization.slice(-1));
-	if (
-		typeof validate == "boolean" ||
-		!validate ||
-		authorization[0] != "Bearer"
-	) {
-		res.status(401).json({
-			status: false,
-			code: 401,
-			message: "access unauthorized",
-			data: null,
-		});
-		return;
-	}
-
-	let account = await Account.findAll({
-		where: {
-			username,
-		},
-		attributes: ["password"],
-	});
-	account = account[0];
-
-	let status = false;
-	if (
-		validate.username == username &&
-		(await checkBcrypt(account.password, reqBody.password))
-	) {
-		await Account.destroy({
-			where: {
-				username,
-				password: account.password,
-			},
-		});
-		status = true;
-	}
-
-	if (!status) {
-		res.status(403).json({
-			status: false,
-			code: 403,
-			message: "forbidden access",
-			data: {
-				errors: ["invalid credentials for account to delete password"],
-			},
-		});
-		return;
-	}
-
-	res.status(200).json({
-		status: true,
-		code: 200,
-		message: "account deleted successfully",
-		data: null,
-	});
-});
+router.delete("/:username", deleteAccountHandler);
 
 module.exports = router;
